4f0daf8bf96c5ea79ed290081043b1dd8bdf4c3a
ASUS <krishseyon01@gmail.com>
2020-08-18 22:40:15 +0530

    seyon commit

---
+++ app/src/main/java/com/example/ada/UpdateCredit.java
@@ -0,0 +1,13 @@
+package com.example.ada;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UpdateCredit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_credit);
+    }
+}
